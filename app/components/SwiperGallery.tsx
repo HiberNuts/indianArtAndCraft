287d5abee5aee54814f17a859821ea5ce3dd62e6
Raghav Jindal <raghavjindal0212@gmail.com>
2024-03-24 19:51:57 +0530

    .

--- app/components/SwiperGallery.tsx
+++ app/components/SwiperGallery.tsx
@@ -4,13 +4,22 @@ import "swiper/css";
 import { Pagination, Autoplay } from "swiper/modules";
 import { simplifiedProduct } from "../interface";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const SwiperGallery = ({ data }: any) => {
+  const [width, setWidth] = useState(0);
+  const handleResize = () => setWidth(window.innerWidth);
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div className="w-full h-[300px] p-10 md:mb-32 mb-24">
+    <div className="w-full h-[300px] p-10 md:mb-36 mb-28">
       <h2 className="text-2xl font-bold tracking-tight text-gray-900">Explore our gallery</h2>
       <Swiper
-        slidesPerView={window.innerWidth < 600 ? 1 : 4}
+        slidesPerView={width < 600 ? 1 : 4}
         spaceBetween={30}
         pagination={{
           clickable: true,
@@ -25,9 +34,14 @@ const SwiperGallery = ({ data }: any) => {
           ?.slice()
           .reverse()
           .map((product: simplifiedProduct, index: number) => (
-            <div className=" h-[300px] flex justify-center align-middle">
+            <div key={index} className=" h-[300px] flex justify-center align-middle">
               <SwiperSlide key={index}>
-                <img src={product.imageUrl} alt="Product image" className="object-contain overflow-hidden" />
+                <img
+                  key={index}
+                  src={product.imageUrl}
+                  alt="Product image"
+                  className="object-contain overflow-hidden"
+                />
               </SwiperSlide>
             </div>
           ))}
