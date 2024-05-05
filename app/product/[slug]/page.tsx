fd4ed1547be19f32f3b7b08497d6b47c1a8eed36
Raghav Jindal <raghavjindal0212@gmail.com>
2024-05-05 23:48:23 +0530

    Added bullet point support

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -7,7 +7,55 @@ import { Button } from "@/components/ui/button";
 import { Star } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { PortableText } from "@portabletext/react";
+import { PortableText, PortableTextComponents } from "@portabletext/react";
+
+const components: PortableTextComponents = {
+  marks: {
+    // Ex. 1: custom renderer for the em / italics decorator
+    em: ({ children }) => <em className="text-gray-600 font-semibold">{children}</em>,
+
+    // Ex. 2: rendering a custom `link` annotation
+    link: ({ value, children }) => {
+      const target = (value?.href || "").startsWith("http") ? "_blank" : undefined;
+      return (
+        <a href={value?.href} target={target}>
+          {children}
+        </a>
+      );
+    },
+  },
+  list: {
+    // Ex. 1: customizing common list types
+    bullet: ({ children }) => <ul className="mt-xl">{children}</ul>,
+    number: ({ children }) => <ol className="mt-lg">{children}</ol>,
+
+    // Ex. 2: rendering custom lists
+    checkmarks: ({ children }) => <ol className="m-auto text-lg">{children}</ol>,
+  },
+  block: {
+    // Ex. 1: customizing common block types
+    h1: ({ children }) => <h1 className="text-2xl">{children}</h1>,
+    h2: ({ children }) => <h2 className="text-xl">{children}</h2>,
+    h3: ({ children }) => <h3 className="text-l">{children}</h3>,
+    h4: ({ children }) => <h4 className="text-base">{children}</h4>,
+    h5: ({ children }) => <h5 className="text-xs">{children}</h5>,
+    blockquote: ({ children }) => <blockquote className="border-l-purple-500">{children}</blockquote>,
+
+    // Ex. 2: rendering custom styles
+    customHeading: ({ children }) => <h2 className="text-lg text-primary text-purple-700">{children}</h2>,
+  },
+  listItem: {
+    // Ex. 1: customizing common list types
+    bullet: ({ children }) => <li style={{ listStyleType: "circle", marginLeft: "20px" }}>{children}</li>,
+    number: ({ children }) => (
+      <li style={{ listStyleType: "decimal", marginLeft: "20px" }} className="mt-lg">
+        {children}
+      </li>
+    ),
+    // Ex. 2: rendering custom list items
+    checkmarks: ({ children }) => <li>✅ {children}</li>,
+  },
+};
 
 async function getData(slug: string) {
   const query = `*[_type == "product" && slug.current == "${slug}"][0] {
@@ -47,7 +95,7 @@ export const dynamic = "force-dynamic";
 
 export default async function ProductPge({ params }: { params: { slug: string } }) {
   const data: any = await getData(params.slug);
-  // console.log(data.content);
+  console.log(data.content);
 
   const Relateddata: simplifiedProduct[] = await getRelatedData();
 
@@ -91,7 +139,7 @@ export default async function ProductPge({ params }: { params: { slug: string } 
         </div>
         {/* <p className="mt-4 w-full text-base text-gray-500 tracking-wide">{data.description}</p> */}
 
-        <PortableText value={data?.content} />
+        <PortableText components={components} listNestingMode="direct" value={data?.content} />
       </div>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="flex justify-between items-center">
